9923a4b73b37f29f4b071311f1b8094da6778584
sahel-abderrahim <sahelrahim95@gmail.com>
2026-09-08 18:29:36 +0100

    first commit

---
+++ scripts.js
@@ -0,0 +1,13 @@
+const langs = ["ab","bc"]
+console.log(langs);
+langs[1] = "cd" ;
+console.log(langs[1]);
+console.log(langs);
+langs.push("df")
+console.log(langs);
+langs.pop("ab")
+console.log(langs);
+console.log(ab);
+
+
+
